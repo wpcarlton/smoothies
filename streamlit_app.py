f4f3ed174d9aefd19bc081e685c7250ba3909681
wpcarlton <williamparkercarlton@gmail.com>
2025-06-11 17:21:05 -0500

    Update streamlit_app.py
    Connected changes needed because of the move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
   """Choose the fruits you want in your custom Smoothie!
   """)
-
 #import streamlit as st
 
 name_on_order = st.text_input("Name on Smoothie: ")
