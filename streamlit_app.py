cc42ebe7c78e163619655772c9deb91cf4c03aeb
wpcarlton <williamparkercarlton@gmail.com>
2025-06-12 12:25:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ if ingredients_list:
         st.success("Your Smoothie is ordered!", icon="✅")
 
 
-  # New section to display smoothiefroot nutrition information
-  import requests
-  smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-  st.text(smoothiefroot_response)
+    # New section to display smoothiefroot nutrition information
+    import requests
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
