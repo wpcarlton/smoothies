fa643080b1a01200a31b3e901be9d9bbf1d079e7
wpcarlton <williamparkercarlton@gmail.com>
2025-06-12 13:08:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-            ingredients_string += fruit_chosen + ' '
+            ingredients_string += search_on + ' '
             st.subheader(fruit_chosen + ' Nutrition Information')
             smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
             sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
